797324418488be8352104e4041d588a1362df69d
gonzalo fernandez <gonchjj.14@gmail.com>
2023-08-30 22:38:44 -0300

    commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
-import Cards from '../Components/Cards/Cards'
-import DetailsCard from '../Components/Cards/DetailsCard'
-=======
 
 import SearchBar from '../Components/Header/SearchBar'
->>>>>>> 54d4541030a6a5b7e960f9f8d50a8fec60244b49
 
 function Home() {
   return (
     <div>
-<<<<<<< HEAD
-      <Cards></Cards>
-      <DetailsCard></DetailsCard>
-=======
       <SearchBar></SearchBar>
  
->>>>>>> 54d4541030a6a5b7e960f9f8d50a8fec60244b49
     </div>
   )
 }
