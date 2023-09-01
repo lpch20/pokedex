9d9fa2e515f1b539ed5b42fd3d1d1d2f89beb0b2
geral <aalvarezgeraldine@gmail.com>
2023-09-01 20:36:04 -0300

    push geral

--- src/Components/Header/SearchBar.jsx
+++ src/Components/Header/SearchBar.jsx
@@ -1,6 +1,8 @@
 import Cards from "../Cards/Cards";
 import "./searchbar.css";
 import { useEffect, useState } from "react";
+import iconNumber from "/icons/tag.svg";
+import iconName from "/icons/text_format.svg";
 
 function SearchBar() {
   const [allPokemon, setAllPokemon] = useState([]);
@@ -9,6 +11,7 @@ function SearchBar() {
   const [id, setPokeid] = useState("");
   const [click, setclick] = useState(false);
   const [checked, setChecked] = useState("Number");
+  const [close, setClose] = useState(false);
 
   var requestOptions = {
     method: "GET",
@@ -19,38 +22,34 @@ function SearchBar() {
     setclick(!click);
   };
 
-  const orderName = (pokemon1, pokemon2)=>{
-    
-      if (pokemon1.name < pokemon2.name) {
-        return -1;
-      }
-      if (pokemon1.name > pokemon2.name) {
-        return 1;
-      }
+  const orderName = (pokemon1, pokemon2) => {
+    if (pokemon1.name < pokemon2.name) {
+      return -1;
+    }
+    if (pokemon1.name > pokemon2.name) {
+      return 1;
+    }
 
-      return 0;
-    
-  }
-  const orderNumber = (pokemon1, pokemon2)=>{
-    
-      if (+pokemon1.id.replace("#", "") < +pokemon2.id.replace("#", "")) {
-        return -1;
-      }
-      if (+pokemon1.id.replace("#", "") > +pokemon2.id.replace("#", "")) {
-        return 1;
-      }
+    return 0;
+  };
+  const orderNumber = (pokemon1, pokemon2) => {
+    if (+pokemon1.id.replace("#", "") < +pokemon2.id.replace("#", "")) {
+      return -1;
+    }
+    if (+pokemon1.id.replace("#", "") > +pokemon2.id.replace("#", "")) {
+      return 1;
+    }
 
-      return 0;
-    
-  }
+    return 0;
+  };
 
   const handleCheck = (option) => {
     setChecked(option);
     let order = allPokemon;
-    if(checked === "Name") {
-      order = allPokemon.sort(orderNumber)
-    }else{
-      order = allPokemon.sort(orderName)
+    if (checked === "Name") {
+      order = allPokemon.sort(orderNumber);
+    } else {
+      order = allPokemon.sort(orderName);
     }
     setAllPokemon(order);
   };
@@ -69,13 +68,15 @@ function SearchBar() {
   useEffect(() => {
     const filtered =
       checked === "Number"
-      ? allPokemon.filter((pokemon) => pokemon.id.includes(id.replace("#", "")))
+        ? allPokemon.filter((pokemon) =>
+            pokemon.id.includes(id.replace("#", ""))
+          )
         : allPokemon.filter((pokemon) =>
             pokemon.name.toLowerCase().includes(name.toLowerCase())
           );
 
     setFilterPokemon(filtered);
-  }, [name, allPokemon,id, checked]);
+  }, [name, allPokemon, id, checked]);
 
   return (
     <div className="search-bar">
@@ -85,23 +86,36 @@ function SearchBar() {
       </div>
       <div className="bar-and-filter">
         <div className="pokedex-search">
-          <img src="/icons/search.svg" alt="" />
+          <img src="/icons/search.svg" alt="" className="img-search" />
           <input
             type="text"
             placeholder="Search"
-            value={checked === "Number" ? id : name} 
+            value={checked === "Number" ? id : name}
             onChange={(e) => {
               if (checked === "Number") {
                 setPokeid(e.target.value);
               } else {
-                setPokename(e.target.value); 
+                setPokename(e.target.value);
+              }
+              if (e.target.value.length !== "") {
+                setClose(true);
+              } else if(e.target.value.length === "") {
+                setClose(false);
               }
             }}
           />
+          {close && (
+            <button className="close" onClick={() => setPokeid("") || setPokename("")}>
+              {id &&(
+              <img src="/icons/close.svg" alt="" className="img-close" />
+            )}
+            </button>
+          )}
         </div>
         <div className="filter">
           <button onClick={handleClick}>
-            <img src="/icons/sort.svg" alt="" />
+            {/* <img src="/icons/sort.svg" alt="" /> */}
+            <img src={checked === "Number" ? iconNumber : iconName} alt="" />
           </button>
         </div>
 
@@ -129,7 +143,7 @@ function SearchBar() {
                   className="circular-checkbox"
                   value="Name"
                   checked={checked === "Name"}
-                  onChange={() => handleCheck("Name") }
+                  onChange={() => handleCheck("Name")}
                 />
                 <label htmlFor="input-name">Name</label>
               </div>
@@ -138,7 +152,6 @@ function SearchBar() {
         ) : null}
       </div>
       <Cards filterPokemon={filterPokemon} />
-      
     </div>
   );
 }
